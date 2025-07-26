a7022203cbfcc68854de368e98a0fa819ddf61cc
lamvinhkien <lamvinhkien1709@gmail.com>
2025-07-26 21:15:33 +0700

    video 57

--- src/server.js
+++ src/server.js
@@ -3,13 +3,17 @@ import express from 'express'
 import exitHook from 'async-exit-hook'
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
+import { APIs_V1 } from '~/routes/v1'
+import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware'
 
 const START_SERVER = () => {
   const app = express()
 
-  app.get('/', async (req, res) => {
-    res.end('<h1>Hello World!</h1><hr>')
-  })
+  app.use(express.json())
+
+  app.use('/v1', APIs_V1)
+
+  app.use(errorHandlingMiddleware)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`Server running at http://${env.APP_HOST}:${env.APP_PORT}`)
