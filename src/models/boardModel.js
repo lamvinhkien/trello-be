6cb6787a74f41079221776431cb699e4dbc0bdbe
lamvinhkien <lamvinhkien1709@gmail.com>
2025-08-03 16:13:15 +0700

    video 67

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -62,7 +62,18 @@ const getDetails = async (id) => {
       }
     ]).toArray()
 
-    return result[0] || {}
+    return result[0] || null
+  } catch (error) { throw new Error(error) }
+}
+
+const pushColumnOrderIds = async (column) => {
+  try {
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOneAndUpdate(
+      { _id: new ObjectId(column.boardId) },
+      { $push: { columnOrderIds: new ObjectId(column._id) } },
+      { returnDocument: 'after' }
+    )
+    return result.value
   } catch (error) { throw new Error(error) }
 }
 
@@ -71,5 +82,6 @@ export const boardModel = {
   BOARD_COLLECTION_SCHEMA,
   createNew,
   findOneById,
-  getDetails
+  getDetails,
+  pushColumnOrderIds
 }
